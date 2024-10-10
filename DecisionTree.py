7582304372de54319a152af3403da03fd5f13cbd
DepressedRamen <timruhland@protonmail.com>
2024-10-10 15:41:10 +0200

    Added method for splitting in DecisionTree.py The method splits the data into two parts according to the best information gain.

--- DecisionTree.py
+++ DecisionTree.py
@@ -8,6 +8,37 @@ class DecisionTree:
         self.max_depth = max_depth #maximum depth of the tree 
         
     #region Private Methods
+    def _split(self, y, X): 
+        """Return the best split of a dataset"""
+        best_information_gain = None
+        best_split_value = None 
+        best_feature_index = None
+        
+        #iterate over all features 
+        for feature_index in range(X.shape[1]):
+            information_gains = []
+            X_column = X[:,feature_index]
+            #determine all possible split values for the current feature 
+            split_values = numpy.unique(X_column)
+            for value in split_values: 
+                #calculate the information gain of the current split
+                information_gains.append(self._information_gain(y, X_column, value))
+                
+            #determine the best split for the current feature
+            best_gain_for_feature = max(information_gains)
+            if best_information_gain is None or best_gain_for_feature > best_information_gain:
+                best_information_gain = best_gain_for_feature
+                best_split_value = split_values[information_gains.index(best_gain_for_feature)]
+                best_feature_index = feature_index
+        
+        #split the dataset according to the best split
+        X_column_best_feature = X[:,best_feature_index]
+        left_indices = [index for index,element in enumerate(X_column_best_feature) if element < best_split_value]
+        right_indices = [index for index,element in enumerate(X_column_best_feature) if element >= best_split_value] 
+        
+        return left_indices, right_indices, best_split_value, best_feature_index
+            
+    
     def _information_gain(self, y, X_column, split_value):
         """Return the information gain of a split"""
         parent_entropy = self._entropy(y)
@@ -62,9 +93,3 @@ class DecisionTree:
         """Return the predictions for the dataset X"""
         return [self._predict(single_input) for single_input in X]
     #endregion
-
-    
-
-        
-    
-    
