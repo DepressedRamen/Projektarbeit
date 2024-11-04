b174b602024a5ed7c78fd44267c2078032a698dc
DepressedRamen <timruhland@protonmail.com>
2024-11-04 19:45:08 +0100

    Bugfix regarding categorical features In the _predict_single_input() method, the code was not handling the case where the feature was categorical. This has been fixed by adding a check for the feature type and using the correct method was added. Additionally a spelling mistake was fixed and the method _predict() was renamed to _predict_single_input() to better reflect its purpose and make it easier to distuingish from the predict() method.

--- DecisionTrees/DecisionTree.py
+++ DecisionTrees/DecisionTree.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
+from numbers import Number
 #Decision Tree with all necessary functions for a decision tree 
 
 class DecisionTree(ABC):
-    def __init__(self, root=None, max_depth=13, intervalls=5, min_samples_split=20):
+    def __init__(self, root=None, max_depth=13, intervals=5, min_samples_split=20):
         """Constructor of the decision tree"""
         self.root = root #root node of the tree
         self.max_depth = max_depth #maximum depth of the tree 
-        self.intervals = intervalls #number of intervalls for numerical features
+        self.intervals = intervals #number of intervalls for numerical features
         self.min_samples_split = min_samples_split #minimum number of samples for a leaf node
       
     #region Abstract Methods  
@@ -22,13 +23,19 @@ class DecisionTree(ABC):
     #endregion
 
     #region Private Methods
-    def _predict(self, single_input):
+    def _predict_single_input(self, single_input):
         """Return the prediction for a single input"""
         node = self.root
         #traverse tree until we reach a leaf
         while not node.is_leaf():
-            if single_input[node.feature_index] < node.split_value:
+            feature_value = single_input[node.feature_index]
+            #check if the feature value is numerical or categorical
+            #if it is numerical we compare the value to the split value and go left if it is smaller than the node value 
+            #if it is categorical we go left if the value is equal to the node value
+            if (isinstance(feature_value, Number) and feature_value < node.split_value) or \
+               (not isinstance(feature_value, Number) and feature_value == node.split_value): 
                 node = node.left_child
+            #otherwise we go to the right child of the node 
             else:
                 node = node.right_child
         #return the value of the leaf as a prediction
@@ -38,7 +45,7 @@ class DecisionTree(ABC):
     #region Public Methods
     def predict(self, X):
         """Return the predictions for the dataset X"""
-        return [self._predict(single_input) for single_input in X]
+        return [self._predict_single_input(single_input) for single_input in X]
     
     def fit(self, X, y): 
         """Fit the decision tree to the dataset"""
